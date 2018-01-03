1758c1db70c1bf1f0e902efd8dfee668662908a8
louisdauvois <louis.dauvois@epitech.eu>
2018-01-03 20:10:31 +0800

    Auto push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
+echo "Starting . . ."
+sleep 10
 hdfs dfs -mkdir -p /usr/local/spark/data/mllib
 hdfs dfs -put /usr/local/spark/data/mllib/sample_linear_regression_data.txt /usr/local/spark/data/mllib/sample_linear_regression_data.txt
